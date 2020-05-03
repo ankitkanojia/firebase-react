494e4e7fb0c4bd8f5131bf70aef2e3f5c0881306
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2020-05-03 17:36:26 +0530

    Integrate firebash link with different version.

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,10 +1,18 @@
-//importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js");
 //importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
-importScripts("https://www.gstatic.com/firebasejs/7.14.2/firebase-app.js");
+// importScripts("https://www.gstatic.com/firebasejs/7.14.2/firebase-app.js");
 // importScripts("https://www.gstatic.com/firebasejs/7.14.2/firebase-analytics.js");
 
 firebase.initializeApp({
- //Your Details
+  messagingSenderId: "893833013962"
+  // apiKey: "AIzaSyDayZmQIhMynkemx_-Lxb2--icCpPWHIqM",
+  // authDomain: "papresupplymobileapp.firebaseapp.com",
+  // databaseURL: "https://papresupplymobileapp.firebaseio.com",
+  // projectId: "papresupplymobileapp",
+  // storageBucket: "papresupplymobileapp.appspot.com",
+  // messagingSenderId: "893833013962",
+  // appId: "1:893833013962:web:098f72a599d0b34d936233",
+  // measurementId: "G-7NWS5F0PKC"
 });
 
 self.addEventListener('notificationclick', function (event) {
